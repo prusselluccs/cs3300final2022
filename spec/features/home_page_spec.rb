37ad21ee4f75896c568f5d5f90779264565cd520
prusselluccs <prussell@uccs.edu>
2022-11-10 22:53:11 +0000

    SimpleCov and Capybara

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,4 +4,5 @@ RSpec.feature "Visiting the HomePage", type: :feature do
   scenario "The visitor should see projects" do
     visit root_path
     expect(page).to have_text("Projects")
+  end
 end
--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -21,38 +21,38 @@ RSpec.feature "Projects", type: :feature do
     end
   end
 
-    context "Update project" do
-      let(:project) { Project.create(title: "Test title", description: "Test content") }
-      before(:each) do
-        visit edit_project_path(project)
-      end
-
-      scenario "should be successful" do
-        within("form") do
-          fill_in "Description", with: "New description content"
-        end
-        click_button "Update Project"
-        expect(page).to have_content("Project was successfully updated")
-      end
-
-      scenario "should fail" do
-        within("form") do
-          fill_in "Description", with: ""
-        end
-        click_button "Update Project"
-        expect(page).to have_content("Description can't be blank")
-      end
+  context "Update project" do
+    let(:project) { Project.create(title: "Test title", description: "Test content") }
+    before(:each) do
+      visit edit_project_path(project)
     end
 
-    context "Remove existing project" do
-      let!(:project) { Project.create(title: "Test title", description: "Test content") }
-      scenario "remove project" do
-        visit projects_path
-        click_link "Destroy"
-        expect(page).to have_content("Project was successfully destroyed")
-        expect(Project.count).to eq(0)
+    scenario "should be successful" do
+      within("form") do
+        fill_in "Description", with: "New description content"
       end
+      click_button "Update Project"
+      expect(page).to have_content("Project was successfully updated")
+    end
+
+    scenario "should fail" do
+      within("form") do
+        fill_in "Description", with: ""
+      end
+      click_button "Update Project"
+      expect(page).to have_content("Description can't be blank")
     end
   end
 
+  context "Remove existing project" do
+    let!(:project) { Project.create(title: "Test title", description: "Test content") }
+    scenario "remove project" do
+      visit projects_path
+      click_link "Destroy"
+      expect(page).to have_content("Project was successfully destroyed")
+      expect(Project.count).to eq(0)
+    end
+  end
 end
+
+
